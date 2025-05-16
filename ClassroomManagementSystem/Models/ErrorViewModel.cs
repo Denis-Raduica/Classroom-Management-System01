e6b2ef41982b72a19681e5d808efb5cba72370cc
Denis-Raduica <raduica.denis.y9i@student.ucv.ro>
2025-05-16 15:15:20 +0300

    Delete ClassroomManagementSystem directory

--- ClassroomManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClassroomManagementSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
